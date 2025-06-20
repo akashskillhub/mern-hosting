6adfe137e427c366670e1644ca802683380873d8
akash@maticui <akash@maticui.com>
2025-06-20 18:43:42 +0530

    env update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+require("dotenv").config()
 const app = express()
 app.use(cors({ origin: "http://localhost:5173" }))
 
 app.use(express.json()) // 👈 body parser
 app.use("/api/notes", require("./routes/todo.routes"))
 
-mongoose.connect("mongodb://localhost:27017/express-todo")
+mongoose.connect(process.env.MONGODB_URL)
 mongoose.connection.once("open", () => {
     console.log("db connected")
-    app.listen(5000, console.log("server running on 5000"))
+    app.listen(process.env.PORT, console.log("server running on 5000"))
 })
